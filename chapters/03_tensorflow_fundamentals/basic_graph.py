c75c6c601643416e581a8a1e78b353e8967645e8
Mo Dongsong <modongsong@126.com>
2017-12-21 14:59:50 +0800

    update

--- chapters/03_tensorflow_fundamentals/basic_graph.py
+++ chapters/03_tensorflow_fundamentals/basic_graph.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 # Build our graph nodes, starting from the inputs
 a = tf.constant(5, name="input_a")
 b = tf.constant(3, name="input_b")
-c = tf.mul(a,b, name="mul_c")
+c = tf.multiply(a,b, name="mul_c")
 d = tf.add(a,b, name="add_d")
 e = tf.add(c,d, name="add_e")
 
